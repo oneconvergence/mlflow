7f3483f92390ca8734535d17a6c7bb22b2209fca
hsoni92 <h.soni1321992@gmail.com>
2025-12-26 17:17:41 +0530

    pytorch-profiler-fix

--- mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactTraceView.tsx
+++ mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactTraceView.tsx
@@ -15,8 +15,7 @@ type ShowArtifactTraceViewState = {
   loading: boolean;
   error?: any;
   path: string;
-  tracedata: any;
-  iframeReady: boolean;
+  tracedata: string;
 };
 
 type ShowArtifactTraceViewProps = {
@@ -42,8 +41,7 @@ class ShowArtifactTraceView extends Component<ShowArtifactTraceViewProps, ShowAr
     loading: true,
     error: undefined,
     path: '',
-    tracedata: null,
-    iframeReady: false,
+    tracedata: '',
   };
 
   componentDidMount() {
@@ -53,8 +51,8 @@ class ShowArtifactTraceView extends Component<ShowArtifactTraceViewProps, ShowAr
 
   componentDidUpdate(prevProps: ShowArtifactTraceViewProps) {
     if (this.props.path !== prevProps.path || this.props.runUuid !== prevProps.runUuid) {
-      this.setState({ iframeReady: false, loading: true, tracedata: null });
       this.fetchArtifacts();
+      window.addEventListener('message', this.traceViewDataHandler, true);
     }
   }
 
@@ -96,66 +94,38 @@ class ShowArtifactTraceView extends Component<ShowArtifactTraceViewProps, ShowAr
       .getArtifact(artifactLocation, true)
       .then((tracebindata: ArrayBufferLike) => {
         const uint8Array = new Uint8Array(tracebindata);
-        let dataString = '';
+        let data = '';
         // Gzip files start with the magic number 0x1f 0x8b
         if (uint8Array[0] === 0x1f && uint8Array[1] === 0x8b) {
           try {
-            dataString = pako.ungzip(uint8Array, { to: 'string' });
+            data = pako.ungzip(uint8Array, { to: 'string' });
           } catch (error) {
             console.error('Decompression error:', error);
             this.setState({ error: error, loading: false, path: this.props.path });
             return;
           }
         } else {
-          dataString = new TextDecoder().decode(uint8Array);
+          data = new TextDecoder().decode(uint8Array);
         }
-
-        // Parse JSON data for the trace viewer
-        let parsedData;
-        try {
-          parsedData = JSON.parse(dataString);
-        } catch (error) {
-          console.error('JSON parse error:', error);
-          this.setState({ error: error, loading: false, path: this.props.path });
-          return;
-        }
-
-        this.setState({ tracedata: parsedData, loading: false, path: this.props.path }, () => {
-          // Send data if iframe is already ready
-          if (this.state.iframeReady) {
-            this.sendDataToIframe();
-          }
-        });
+        this.setState({ tracedata: data, loading: false, path: this.props.path });
       })
       .catch((error: Error) => {
         this.setState({ error: error, loading: false, path: this.props.path });
       });
   }
 
-  sendDataToIframe() {
-    if (this.iframeRef.current && this.iframeRef.current.contentWindow && this.state.tracedata) {
-      const fileName = this.props.path.split('/').pop() || 'unknown';
-      this.iframeRef.current.focus();
-      this.iframeRef.current.contentWindow.postMessage(
-        { msg: 'data', name: fileName, data: this.state.tracedata },
-        '*'
-      );
-    }
-  }
-
   traceViewDataHandler(event: MessageEvent) {
-    const data = event.data || {};
+    const data = event.data || {}
     if (data.msg === 'ready') {
-      this.setState({ iframeReady: true }, () => {
-        // Send data if it's already loaded (use current state after update)
-        const currentState = this.state;
-        if (currentState.tracedata && !currentState.loading) {
-          this.sendDataToIframe();
-        }
-      });
+      if (this.iframeRef.current && this.iframeRef.current.contentWindow) {
+        this.iframeRef.current.focus();
+        this.iframeRef.current.contentWindow.postMessage(
+          { msg: 'data', data: this.state.tracedata },
+          '*'
+        );
+      }
     }
   }
 }
 
 export default ShowArtifactTraceView;
-
